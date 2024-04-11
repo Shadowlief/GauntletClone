1d48a6830c0a6af488174c95182ec9663c34e1f0
Justin Lam <jlam123321@gmail.com>
2024-04-11 13:40:07 -0700

    started on items

---
+++ Gauntlet/Assets/Scripts/Item/IItemVisitor.cs
@@ -0,0 +1,10 @@
+/*
+ * Author: [Lam, Justin]
+ * Last Updated: [04/11/2024]
+ * [Interface for each element of a Character]
+ */
+
+public interface IItemVisitor
+{
+    void Visit(PlayerData playerData);
+}
