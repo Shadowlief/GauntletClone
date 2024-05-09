d95d92ec020f5df9c8ed8512500f4a4ec38e2d04
Shadowlief <103529542+Shadowlief@users.noreply.github.com>
2024-05-09 13:51:48 -0700

    Smoll Updates
    -tweaked the proper level 1
    -finished laying out lv2

--- Gauntlet/Assets/Scripts/SceneSwitch.cs
+++ Gauntlet/Assets/Scripts/SceneSwitch.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -16,7 +17,7 @@ public class SceneSwitch : MonoBehaviour
 
     /// <summary>
     /// If a player collides with the hole
-    /// the player falls into the hole (and is currently disabled)
+    /// the player falls into the hole (and is currently out of the way)
     /// If the amount of players that have fallen into the hole is equal to the amount of players
     /// Load the next scene
     /// </summary>
@@ -27,6 +28,7 @@ public class SceneSwitch : MonoBehaviour
         {
             Debug.Log("Player Count: " + PlayerManager.Instance.GetPlayerCount());
             Debug.Log("Down the hole!");
+            other.transform.position = new Vector3(other.transform.position.x, other.transform.position.y -100, other.transform.position.z);
             _playersInHole++;
             if(PlayerManager.Instance.GetPlayerCount() == _playersInHole)
             {
